68e7da9730dc9227f48e2d79dafbbe69b902723d
JHORJE18 <wiijlg@hotmail.com>
2025-01-23 18:06:33 +0100

    Test automatizados

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -2,74 +2,58 @@ import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
 
-describe('App Component', () => {
+describe('Juego completo', () => {
 
-    it("muestra y oculta el SplashScreen correctamente", async () => {
+    beforeEach(async () => {
         render(<App />);
 
-        // Verifica que el SplashScreen se muestra inicialmente
-        expect(screen.getByText(/🎲/i)).toBeInTheDocument();
-
-        // Espera a que el SplashScreen desaparezca
+        // Esperar a que el SplashScreen desaparezca
         await waitFor(() => {
             expect(screen.queryByTestId("splash-emoji")).not.toBeInTheDocument();
-        }, { timeout: 1500 });
+        }, { timeout: 2000 });
+    })
+
+
+    it("Muestra y oculta el SplashScreen correctamente", async () => {
+        // Verifica que el SplashScreen se muestra inicialmente
+        expect(screen.getByText(/🎲/i)).toBeInTheDocument();
 
         // Verifica que el contenido principal se muestra
         expect(screen.getByText(/Cubo de Poker/i)).toBeInTheDocument();
     });
 
-    it('adds a new player after SplashScreen', async () => {
-        render(<App />);
-
-        // Esperar a que el SplashScreen desaparezca
-        await waitFor(() => {
-            expect(screen.queryByTestId("splash-emoji")).not.toBeInTheDocument();
-        }, { timeout: 1500 });
-
+    it('Añade un nuevo jugador', async () => {
         // Interactuar con el formulario
-        const input = screen.getByPlaceholderText(/Introduce un jugador/i);
+        const input = screen.getByPlaceholderText(/Nuevo jugador/i);
         const button = screen.getByText(/Añadir jugador/i);
 
-        await userEvent.type(input, 'Jugador 1');
+        await userEvent.type(input, 'Jugador A');
         await userEvent.click(button);
 
-        // Verificar que el jugador fue añadido
-        const newPlayer = screen.getByText(/Jugador 1/i);
-        expect(newPlayer).toBeInTheDocument();
+        // Verificar que el jugador fue añadido en la tabla
+        const newPlayer = screen.getAllByText(/Jugador A/i);
+        const table = screen.getByRole('table');
+        const playerInTable = newPlayer.some(player => table.contains(player));
+        expect(playerInTable);
     });
 
     it('disables the delete button when no players exist', async () => {
-        render(<App />);
-
-        // Esperar a que el SplashScreen desaparezca
-        await waitFor(() => {
-            expect(screen.queryByTestId("splash-emoji")).not.toBeInTheDocument();
-        }, { timeout: 1500 });
-
         // Verificar que el botón de eliminar jugador está deshabilitado
         const deleteButton = screen.getByText(/Eliminar jugador/i);
         expect(deleteButton).toBeDisabled();
     });
 
-    it('removes a player correctly', async () => {
-        render(<App />);
-
-        // Esperar a que el SplashScreen desaparezca
-        await waitFor(() => {
-            expect(screen.queryByTestId("splash-emoji")).not.toBeInTheDocument();
-        }, { timeout: 1500 });
-
+    it('Elimina a un jugador', async () => {
         // Añadir un jugador
-        const input = screen.getByPlaceholderText(/Introduce un jugador/i);
+        const input = screen.getByPlaceholderText(/Nuevo jugador/i);
         const addButton = screen.getByText(/Añadir jugador/i);
 
-        await userEvent.type(input, 'Jugador 1');
+        await userEvent.type(input, 'Jugador B');
         await userEvent.click(addButton);
 
         // Verificar que el jugador fue añadido
-        const newPlayer = screen.getByText(/Jugador 1/i);
-        expect(newPlayer).toBeInTheDocument();
+        const newPlayer = screen.getAllByText(/Jugador B/i);
+        expect(newPlayer[0]).toBeInTheDocument();
 
         // Eliminar el jugador
         const deleteButton = screen.getByText(/Eliminar jugador/i);
@@ -79,30 +63,36 @@ describe('App Component', () => {
         expect(screen.queryByText(/Jugador 1/i)).not.toBeInTheDocument();
     });
 
-    it('updates player score correctly', async () => {
-        render(<App />);
-
-        // Esperar a que el SplashScreen desaparezca
-        await waitFor(() => {
-            expect(screen.queryByTestId("splash-emoji")).not.toBeInTheDocument();
-        }, { timeout: 1500 });
-
+    it('Actualiza la puntuación del jugador correctamente', async () => {
         // Añadir un jugador
-        const input = screen.getByPlaceholderText(/Introduce un jugador/i);
+        const input = screen.getByPlaceholderText(/Nuevo jugador/i);
         const addButton = screen.getByText(/Añadir jugador/i);
 
-        await userEvent.type(input, 'Jugador 1');
+        await userEvent.type(input, 'Jugador C');
         await userEvent.click(addButton);
 
         // Verificar que el jugador fue añadido
-        const newPlayer = screen.getByText(/Jugador 1/i);
-        expect(newPlayer).toBeInTheDocument();
+        const newPlayer = screen.getAllByText(/Jugador C/i);
+        expect(newPlayer[0]).toBeInTheDocument();
 
         // Actualizar la puntuación del jugador
-        const scoreInput = screen.getByLabelText(/As/i);
-        await userEvent.type(scoreInput, '5');
+        const scoreInput = screen.getAllByLabelText('As:');
+        await userEvent.type(scoreInput[0], '5');
 
         // Verificar que la puntuación fue actualizada
-        expect(scoreInput).toHaveValue(5);
+        expect(scoreInput[0]).toHaveValue(5);
+
+        // Verificar puntuación en la tabla
+        const table = screen.getByRole('table');
+        const scoreInTable = screen.getAllByText(/30/i);
+        expect(table.contains(scoreInTable[0])).toBeTruthy();
+
+        // Añade puntuación a ronda 2
+        await userEvent.type(scoreInput[1], '3');
+        expect(scoreInput[1]).toHaveValue(3);
+
+        // Verifica que la puntuación fue actualizada
+        expect(screen.getByText(/18/i)).toBeInTheDocument();
+        expect(scoreInTable[1]).toHaveTextContent('48');
     });
 });
